016024f0c9532767142712f383cb630ad8409f8f
Sanjay Purswani <sanjsanj@hotmail.com>
2019-05-06 10:43:14 +0100

    Add starting GlobalStyles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    font-family: Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    box-sizing: border-box;
+    min-height: 100vh;
+  }
+  body {
+    margin: auto;
+  }
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+`;
 
 function App() {
-  return <div>App...</div>;
+  return (
+    <div>
+      <GlobalStyles />
+
+      <h1>App...</h1>
+    </div>
+  );
 }
 
 export default App;
